b0a59bb9598e9b38f62ecdb86abd476e15013ee3
dhirajs1997 <32067769+dhirajs1997@users.noreply.github.com>
2018-03-19 20:25:07 +0530

    Fixed repo link

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -6,7 +6,7 @@
           <strong>BrowserTorrent</strong> by Dhiraj, Umesh, and Anish.
         </p>
         <p>
-          This program is <strong>open source</strong>.<a href="https://github.com/dhirajs1997/Projects/tree/master/Browser%20Torrent" target="_blank"> Help us improve this program on Github.</a>
+          This program is <strong>open source</strong>.<a href="https://github.com/dhirajs1997/BrowserTorrent" target="_blank"> Help us improve this program on Github.</a>
         </p>
         <p>Donate us on Ethereum at : <a>0x87B560DbE9958839995f33B773cd389B666e11fD</a></p>
       </div>
